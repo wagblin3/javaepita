ae26b521ae7de7ded71fc6df1e1d2d1e8b85fb74
wagblin3 <wagblin3@gmail.com>
2022-10-20 17:38:30 +0200

    fin de journée de jeudi

---
+++ src/boucles/DessinerRectangle.java
@@ -0,0 +1,21 @@
+package boucles;
+
+public class DessinerRectangle {
+
+    public static void main(String[] args) {
+        DessinerRectangle app = new DessinerRectangle();
+        app.dessiner(5, 3);
+    }
+
+    public void dessiner(int longueur, int largeur){
+
+        for ( int i = 0; i < largeur; i++){
+            for ( int j = 0; j < longueur; j++){
+                System.out.print("0");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
