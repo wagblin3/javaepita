ca97b9c5a7c0e1b05eb0b13f88b1dc19266c1efb
wagblin3 <wagblin3@gmail.com>
2022-10-21 12:33:08 +0200

    fin de matinée vendredi

---
+++ src/constructeurs/Main.java
@@ -0,0 +1,18 @@
+package constructeurs;
+
+public class Main {
+    public static void main(String[] args) {
+        Enfant enfant1 = new Enfant("dupont", "charlotte");
+        Enfant enfant2 = new Enfant("martin", "jerome");
+        enfant1.presentation();
+        enfant2.presentation();
+        enfant1.feterAnniversaire();
+        enfant1.feterAnniversaire();
+        enfant1.feterAnniversaire();
+        enfant1.feterAnniversaire();
+        enfant1.feterAnniversaire();
+        enfant1.presentation();
+        enfant2.presentation();
+    }
+
+}
