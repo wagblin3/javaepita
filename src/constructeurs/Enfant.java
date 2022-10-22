2f5f2b22cb019fa5ae2563ee0292ad2390ea6379
wagblin3 <wagblin3@gmail.com>
2022-10-22 09:50:57 +0200

    fin de journée du vendredi 21/10

--- src/constructeurs/Enfant.java
+++ src/constructeurs/Enfant.java
@@ -1,40 +1,31 @@
 package constructeurs;
-
 public class Enfant {
     String nom;
     String prenom;
     int age;
-
     public Enfant(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
         this.age = 0;
     }
-
     public String getNom() {
         return nom;
     }
-
     public void setNom(String nom) {
         this.nom = nom;
     }
-
     public String getPrenom() {
         return prenom;
     }
-
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
-
     public void presentation() {
         System.out.println("Bonjour, je m'appelle "+prenom+" "+nom+". J'ai "+age+" ans");
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public void feterAnniversaire() {
         if (age<17){
             age++;
